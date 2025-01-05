4439e7926d6e0c63a3ee11295d7e8f706c5ea0ae
Stephen Harris <contact@stephenharris.info>
2025-01-05 01:57:46 +0000

    Remove unused import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,6 @@
-import {
-  IonFooter
-  } from '@ionic/react';
 import './Footer.css';
 
-
 const Footer: React.FC = () => {
-
-
   return (
     <p className='app-footer'>
                Version: {process.env.REACT_APP_GIT_SHA}
