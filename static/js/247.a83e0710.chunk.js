6c80d65df1a27386b1c25f1348df2362086d2230
stephenharris <contact@stephenharris.info>
2025-07-14 14:13:28 +0000

    Deploying to gh-pages from @ stephenharris/task-app@61117ad118a7b006ddd8ed530f17f5676f36b3fc 🚀

---
+++ static/js/247.a83e0710.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 247.a83e0710.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunktodo=self.webpackChunktodo||[]).push([[247],{2247:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var o=n(2108),s=n(7721),r=n(9384);const a=()=>{const e=window;e.addEventListener("statusTap",()=>{(0,o.gv)(()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,s.a)(a);i&&new Promise(e=>(0,r.c)(i,e)).then(()=>{(0,o.bN)(async()=>{i.style.setProperty("--overflow","hidden"),await(0,s.s)(i,300),i.style.removeProperty("--overflow")})})})})}}}]);
+//# sourceMappingURL=247.a83e0710.chunk.js.map
